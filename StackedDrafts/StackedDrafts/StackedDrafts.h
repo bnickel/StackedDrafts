3c4ea833910985053db07ea64ce1a6e71c3190c9
Brian Nickel <bnickel@stackexchange.com>
2016-04-21 13:20:39 -0700

    Initial commit

---
+++ StackedDrafts/StackedDrafts/StackedDrafts.h
@@ -0,0 +1,19 @@
+//
+//  StackedDrafts.h
+//  StackedDrafts
+//
+//  Created by Brian Nickel on 4/21/16.
+//  Copyright © 2016 Stack Exchange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StackedDrafts.
+FOUNDATION_EXPORT double StackedDraftsVersionNumber;
+
+//! Project version string for StackedDrafts.
+FOUNDATION_EXPORT const unsigned char StackedDraftsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackedDrafts/PublicHeader.h>
+
+
